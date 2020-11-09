37b2a2dcdecc03a40c7f4155c73cd2792c80e195
linwenjun <linwenjun@163.com>
2020-11-09 09:32:05 +0000

    first commit

---
+++ EFCoreMigrationPractice/Models/CompanyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigrationPractice.Models
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EFCoreMigrationPractice/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreMigrationPractice.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
